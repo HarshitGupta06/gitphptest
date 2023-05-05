828db8287cd32ed056cf6d5a2cabaf8b484740f9
Harshit Gupta <39774908+HarshitGupta06@users.noreply.github.com>
2023-05-05 10:53:51 +0530

    Update index.html

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
+<!-- <?php
 
 echo 'Hello World! from '. $_SERVER['REMOTE_ADDR'];
 
 ?>
+ -->
+
+<!DOCTYPE html>
+ 
+<html>
+    <head>
+        <title>
+            Hello World!
+        </title>
+    </head>
+ 
+    <body>
+        echo 'Hello World! from '. $_SERVER['REMOTE_ADDR'];
+    </body>
+</html>
