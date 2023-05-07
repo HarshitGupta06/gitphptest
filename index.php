a7cc07da36740c9004bfc537db1d05d1c01f28ee
Harshit Gupta <39774908+HarshitGupta06@users.noreply.github.com>
2023-05-07 14:33:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-echo 'Hello World! from '. $_SERVER['HTTP_CLIENT_IP'];
+echo 'Hello World! from '. $_SERVER['SERVER_ADDR'];
+echo gethostname();
+$ip = exec("curl https://checkip.amazonaws.com");
+echo "Public IP address: " . $ip;
 
 ?>
 
